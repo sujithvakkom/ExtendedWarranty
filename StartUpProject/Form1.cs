408cc9351fcf83d2a7530fc4021469e367cb0692
Sujith Radhakrishnan <sujithvakkom@gmail.com>
2018-05-27 09:09:49 +0400

    Update

---
+++ StartUpProject/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+using MyExtentions.Controls.MessageBoxExtentions;
+
+namespace StartUpProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DetailedDialog dia = new DetailedDialog(new PropertyGrid())
+            {
+                Text = "Detailed Dialog",
+                Message="Message Summary",
+                Details ="Detailed Message",
+                StartPosition=FormStartPosition.CenterParent
+            };
+            dia.ShowDialog();
+        }
+    }
+}
